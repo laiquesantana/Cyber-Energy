6af7dd5b3c7b4ece2fda0d520d89f70dae5439af
laique <laiquesantana@gmail.com>
2024-11-03 19:05:46 -0300

    Refactor service provider and API class, and bind SaveChatHistoryGateway to ChatHistoryRepository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use App\Adapters\ConfigAdapter;
+use App\Repositories\ChatHistoryRepository;
 use Illuminate\Support\ServiceProvider;
 use Saas\Project\Dependencies\Config\ConfigInterface;
+use Saas\Project\Modules\OpenAi\Chat\Creation\Gateways\SaveChatHistoryGateway;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +18,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(ConfigInterface::class, ConfigAdapter::class);
+        $this->app->bind(SaveChatHistoryGateway::class, ChatHistoryRepository::class);
 
     }
 
