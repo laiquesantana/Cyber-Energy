af055f322a25a62cd60f2eed86d2c24e9c487cf4
laique <laiquesantana@gmail.com>
2024-11-03 20:11:02 -0300

    Refactor service provider and API class, and bind DeleteChatHistoryGateway, UpdateChatHistoryGateway, and RetrieveChatHistoryGateway to ChatHistoryRepository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,9 @@ use App\Repositories\ChatHistoryRepository;
 use Illuminate\Support\ServiceProvider;
 use Saas\Project\Dependencies\Config\ConfigInterface;
 use Saas\Project\Modules\OpenAi\Chat\Creation\Gateways\SaveChatHistoryGateway;
+use Saas\Project\Modules\OpenAi\Chat\Delete\Gateways\DeleteChatHistoryGateway;
+use Saas\Project\Modules\OpenAi\Chat\Find\Gateways\RetrieveChatHistoryGateway;
+use Saas\Project\Modules\OpenAi\Chat\Update\Gateways\UpdateChatHistoryGateway;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(ConfigInterface::class, ConfigAdapter::class);
         $this->app->bind(SaveChatHistoryGateway::class, ChatHistoryRepository::class);
+        $this->app->bind(DeleteChatHistoryGateway::class, ChatHistoryRepository::class);
+        $this->app->bind(UpdateChatHistoryGateway::class, ChatHistoryRepository::class);
+        $this->app->bind(RetrieveChatHistoryGateway::class, ChatHistoryRepository::class);
 
     }
 
