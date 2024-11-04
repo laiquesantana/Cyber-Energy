061bba4fe3b235e29285a4efed5ac09f1babd352
laique <laiquesantana@gmail.com>
2024-11-03 22:03:00 -0300

    Add JwtMiddleware for token authentication

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
+class JwtMiddleware extends BaseMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            // Attempt to authenticate the user using the token
+            $user = JWTAuth::parseToken()->authenticate();
+
+            if (!$user) {
+                return response()->json(['message' => 'User not found'], 404);
+            }
+        } catch (TokenExpiredException $e) {
+            return response()->json(['message' => 'Token has expired'], 401);
+        } catch (TokenInvalidException $e) {
+            return response()->json(['message' => 'Token is invalid'], 401);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'Authorization token not found'], 401);
+        }
+
+        return $next($request);
+    }
+}
