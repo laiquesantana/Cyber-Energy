6d38d652aeaea7ab6cc9fee76e9450a896718544
laique <laiquesantana@gmail.com>
2024-11-04 13:20:37 -0300

    Refactor cors.php to support credentials in the API

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,18 +1,43 @@
+// src/components/App.jsx
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './Layout';
-import ChatApp from './ChatApp';
-import ChatHistory from './ChatHistory';
+import Login from './Login';
+import Register from './Register';
+import ChatApp from './ChatApp'; // The chat component you already have
+import ChatHistory from './ChatHistory'; // If you have a history component
+import PrivateRoute from './PrivateRoute'; // We'll create this next
+import Layout from './Layout'; // If you have a layout component
 
 const App = () => {
     return (
         <Router>
-            <Layout>
-                <Routes>
-                    <Route path="/" element={<ChatApp />} />
-                    <Route path="/history" element={<ChatHistory />} />
-                </Routes>
-            </Layout>
+            <Routes>
+                <Route path="/" element={<Login />} /> {/* Default to Login */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                {/* Protected Routes */}
+                <Route
+                    path="/chat"
+                    element={
+                        <PrivateRoute>
+                            <Layout>
+                                <ChatApp />
+                            </Layout>
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/history"
+                    element={
+                        <PrivateRoute>
+                            <Layout>
+                                <ChatHistory />
+                            </Layout>
+                        </PrivateRoute>
+                    }
+                />
+            </Routes>
         </Router>
     );
 };
